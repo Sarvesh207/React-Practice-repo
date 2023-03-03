9aad05628070cdfcbf7512f25ff7ee078cd7c0b2
Sarvesh207 <sarveshgaynar@gmail.com>
2023-03-03 16:15:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Counter from './components/Counter';
-import './App.css';
-import Fruits from './components/Fruits';
-import Text from './components/Text';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <Text></Text>
+     
     </div>
   );
 }
